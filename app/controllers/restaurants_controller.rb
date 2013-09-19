189ba01a029decb3bee87ae636ef2760fd032b1f
bhawkeswood <bhawkeswood@gmail.com>
2013-09-19 14:14:36 -0400

    adding remaining methods, but getting routes error

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,22 +15,38 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    
-    @restaurant.save
-    redirect_to restaurants_path
+
+      if @restaurant.save
+        redirect_to restaurants_url
+      else
+        render 'new'
+      end
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(params[:product])
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :price_range:, :menu_url)
+    params.require(:restaurant).permit(:name, :address, :price_range, :menu_url)
   end
+
 end
+
